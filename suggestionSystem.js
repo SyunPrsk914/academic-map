f4f3b2e12625ad037ee16b9e4acb7a382fe7f6c3
SyunPrsk914 <syunsyun080914@gmail.com>
2026-04-07 13:59:02 +0900

    Update suggestionSystem.js

--- suggestionSystem.js
+++ suggestionSystem.js
@@ -1,1 +1,44 @@
+document.getElementById("suggest-btn").onclick=()=>{
 
+document.getElementById("suggest-panel").classList.add("open")
+
+}
+
+document.getElementById("close-suggest").onclick=()=>{
+
+document.getElementById("suggest-panel").classList.remove("open")
+
+}
+
+document.getElementById("submit-suggestion").onclick=()=>{
+
+const name=document.getElementById("s-name").value
+const type=document.getElementById("s-type").value
+const parent=document.getElementById("s-parent").value
+const description=document.getElementById("s-description").value
+
+if(!name || !parent){
+
+document.getElementById("suggest-msg").innerText="Missing fields"
+return
+
+}
+
+const suggestion={
+
+id:name,
+type:type,
+parent:parent,
+description:description
+
+}
+
+let suggestions=JSON.parse(localStorage.getItem("suggestions")||"[]")
+
+suggestions.push(suggestion)
+
+localStorage.setItem("suggestions",JSON.stringify(suggestions))
+
+document.getElementById("suggest-msg").innerText="Suggestion saved"
+
+}
