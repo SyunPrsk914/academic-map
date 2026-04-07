9beb2b72001fe008e4c9a4dfe321c449f5ba7c81
SyunPrsk914 <syunsyun080914@gmail.com>
2026-04-07 13:58:10 +0900

    Update search.js

--- search.js
+++ search.js
@@ -1,1 +1,27 @@
+function searchNode(term){
 
+term=term.toLowerCase()
+
+const result=nodes.find(n=>n.id.toLowerCase().includes(term))
+
+if(!result) return
+
+zoomToNode(result)
+
+}
+
+function zoomToNode(node){
+
+const scale=1.7
+
+const transform=d3.zoomIdentity
+.translate(window.innerWidth/2-scale*node.x,
+window.innerHeight/2-scale*node.y)
+.scale(scale)
+
+d3.select("svg")
+.transition()
+.duration(700)
+.call(d3.zoom().transform,transform)
+
+}
